f5fda2cc4502f6946357633ddb40eab7e670fbaf
dosubeen <dosubeen1204@gmail.com>
2021-04-20 23:55:40 +0900

    자바의 여러가지 연산자 연습문제

---
+++ src/OperationTest7.java
@@ -0,0 +1,10 @@
+public class OperationTest7 {
+    public static void main(String[] args) {
+
+        int num = 10;
+        int num2= 20;
+
+        int result = (num>=10) ? num2 +10 : num2 -10;
+        System.out.println(result);  //30
+    }
+}
