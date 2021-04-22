698b665059ab6f1d5b6d2d3e88a78c4b0b3e5f73
dosubeen <dosubeen1204@gmail.com>
2021-04-23 00:06:28 +0900

    for문예제 1부터10까지 더하기

---
+++ src/ForEx.java
@@ -0,0 +1,11 @@
+public class ForEx {
+    public static void main(String[] args) {
+        int i;
+        int sum=0;
+
+        for(i=1;i<=10;i++ ) {
+            sum+=i;
+        }
+        System.out.println("1부터 10까지의 합은" + sum + "입니다.");
+    }
+}
