5f8319d919ea505389c45386d736656a65c5f717
dosubeen <dosubeen1204@gmail.com>
2021-04-22 23:46:30 +0900

    while문 예제-1분복습

---
+++ src/WhileEx2.java
@@ -0,0 +1,12 @@
+public class WhileEx2 {
+    public static void main(String[] args) {
+        int num = 1;
+        int sum = 0;
+
+        while(num <=50){
+            sum+=num;
+            num++;
+        }
+        System.out.println("1부터 50까지의 합은" + sum+ "입니다.");
+    }
+}
