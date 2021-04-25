79ac6a07d63cedc312becc8efa82978633d1131e
dosubeen <dosubeen1204@gmail.com>
2021-04-25 23:42:36 +0900

    1부터 100까지 수 중에 3의배수만 출력 continue문 사용

---
+++ src/ContinueEx2.java
@@ -0,0 +1,11 @@
+public class ContinueEx2 {
+    public static void main(String[] args) {
+        int num;
+
+        for(num=1;num<=100;num++) {  //100까지 반복
+            if (num % 3 != 0)          // num 값이 3의배 인 경우
+                continue;
+            System.out.println(num);
+        }
+    }
+}
