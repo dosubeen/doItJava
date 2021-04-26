0a9852d9aa03564b24c2119ffe751784c5960981
dosubeen <dosubeen1204@gmail.com>
2021-04-26 23:32:38 +0900

    변수 명칭 변경

--- src/Ch4Q4.java
+++ src/Ch4Q4.java
@@ -1,21 +1,21 @@
 public class Ch4Q4 {
     public static void main(String[] args) {
-        int linecount = 4;
-        int spacecount = linecount / 2 + 1;
-        int starcount = 1;
+        int lineCount = 4;
+        int spaceCount = lineCount / 2 + 1;
+        int starCount = 1;
 
-        for (int i = 0; i < linecount; i++) {
-            for (int j = 0; j < spacecount; j++) {
+        for (int i = 0; i < lineCount; i++) {
+            for (int j = 0; j < spaceCount; j++) {
                 System.out.print(' ');
             }
-            for (int j = 0; j < starcount; j++) {
+            for (int j = 0; j < starCount; j++) {
                 System.out.print('*');
             }
-            for (int j = 0; j < spacecount; j++) {
+            for (int j = 0; j < spaceCount; j++) {
                 System.out.print(' ');
             }
-            spacecount -= 1;
-            starcount += 2;
+            spaceCount -= 1;
+            starCount += 2;
             System.out.println();
 
         }
