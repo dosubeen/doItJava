56220e7e19235f458783e985f4010d26b0a79371
dosubeen <dosubeen1204@gmail.com>
2021-04-27 00:37:05 +0900

    학생이름을 반환하는 메서드& 학생이름을 반환하는 메서드

--- src/classpart/Student.java
+++ src/classpart/Student.java
@@ -5,4 +5,14 @@ public class Student {
     String studentName; //학생이름
     int grade;  //학년
     String address; // 사는 곳   ,  모두 멤버 변수
+
+    public void showStudentInfo() {
+        System.out.println(studentName + " , " + address);  //이름 ,주소 출력  (메서드 추가)
+    }
+    public String getStudentName() {
+        return studentName;   //학생의 이름을 반환하는 메서드
+    }
+    public void setStudentName(String name) {
+        studentName = name;
+    }//학생이름을 매개변수로 전달
 }
