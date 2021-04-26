bef951fb88745db4852b5ba2d8102eeb5bd4708e
dosubeen <dosubeen1204@gmail.com>
2021-04-27 00:49:07 +0900

    Student 클래스에 main 함수 추가하기

--- src/classpart/Student.java
+++ src/classpart/Student.java
@@ -12,6 +12,14 @@ public class Student {
     public String getStudentName() {
         return studentName;   //학생의 이름을 반환하는 메서드
     }
+
+    public static void main(String[] args) {
+        Student studentAhn = new Student(); //student 클래스 생성
+        studentAhn.studentName = "안연수";
+
+        System.out.println(studentAhn.studentName);
+        System.out.println(studentAhn.getStudentName());
+    }
     public void setStudentName(String name) {
         studentName = name;
     }//학생이름을 매개변수로 전달
