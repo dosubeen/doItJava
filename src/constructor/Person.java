f444e9a4677ac2c50057c08a636c658cbc47bcbe
dosubeen <dosubeen1204@gmail.com>
2021-04-28 15:25:38 +0900

    생성자 오버로드

--- src/constructor/Person.java
+++ src/constructor/Person.java
@@ -7,9 +7,14 @@ public class Person {
 
      public Person(){}  //자바 컴파일러가 자동으로 제공하는 디폴트 생성자
 
-     /* public Person(String pname){
+     public Person(String pname){
         name = pname;
-    }*/  //  사람 이름을 매개변수로 입력 받아서 Person 클래스를 생성하는 생성자
+    }  //  사람 이름을 매개변수로 입력 받아서 Person 클래스를 생성하는 생성자
 
+    public Person(String pname, float pheight, float pweight) {
+         name = pname;
+         height = pheight;
+         weight = pweight;
+    }  //이름,키,몸무게를 매개변수로 입력 받는 생성자
 
 }
--- src/constructor/PersonTest.java
+++ src/constructor/PersonTest.java
@@ -2,6 +2,13 @@ package constructor;
 
 public class PersonTest {
     public static void main(String[] args) {
-        Person personLee = new Person();  //생성자
-    }
+        Person personkim = new Person();  //생성자
+        personkim.name ="김유신";
+        personkim.height = 180.0F;
+        personkim.weight = 85.5F;
+
+        Person personLee = new Person("이순신",175,75);  // 인스턴스 변수 초기화와 동시에 클래스 생성
+    }  //디폴트 생성자로 클래스를 생성한 후 인스턴스 변수 값을 따로 초기화
+
+
 }
