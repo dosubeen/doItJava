b4c3d2e2482e549bfa5b01eb6413668a49ae1c10
dosubeen <dosubeen1204@gmail.com>
2021-04-28 15:02:03 +0900

    생성자-디폴트생성자

---
+++ src/constructor/Person.java
@@ -0,0 +1,15 @@
+package constructor;
+
+public class Person {
+    String name;
+    float height;
+    float weight;
+
+     public Person(){}  //자바 컴파일러가 자동으로 제공하는 디폴트 생성자
+
+     /* public Person(String pname){
+        name = pname;
+    }*/  //  사람 이름을 매개변수로 입력 받아서 Person 클래스를 생성하는 생성자
+
+
+}
