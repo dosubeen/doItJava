8240c423361a45bffa61a979d6409b1eb8b4385a
dosubeen <dosubeen1204@gmail.com>
2021-04-28 15:50:52 +0900

    참조자료형

---
+++ src/reference/Student2.java
@@ -0,0 +1,10 @@
+package reference;
+
+public class Student2 {
+    int studentID;
+    String studentName;
+    int koreaScore;
+    int mathScore;
+    String koreaSubject;
+    String mathSubject;   //과목이름 변수추가
+}
---
+++ src/reference/Student3.java
@@ -0,0 +1,8 @@
+package reference;
+
+public class Student3 {
+    int StudentID;
+    String studentName;
+    Subject korea;
+    Subject math;   //subject형을 사용하여 선언
+}
